cbaa0306409763979aabfb07c257f4f694b660e6
Danil <dinkodanil@gmail.com>
2021-03-28 19:35:57 +0300

    s

--- dist/index.js
+++ dist/index.js
@@ -3,10 +3,24 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
 const hostname = '127.0.0.1';
 const port = '8000';
+const routing = {
+    '/': 'Hello world!',
+    '/obj': { name: "Oleg", age: 20 },
+    '/func': (req, res) => {
+        return { url: req.url, cookie: req.headers.cookie };
+    }
+};
+const types = {
+    object: JSON.stringify,
+    string: s => s,
+    number: n => n.toString(),
+    undefined: () => '404',
+    function: (fn, req, res) => JSON.stringify(fn(req, res))
+};
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    const data = routing[req.url];
+    const result = types[typeof data](data, req, res);
+    res.end(result);
 });
 server.listen(port, () => {
     console.log(`Server has started on http://${hostname}:${port}/`);
